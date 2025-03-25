e55e2c6f57a55deb7f5e3cf4eca21780c521305d
Frederick John Milens III <frederick@milens3.com>
2025-03-25 03:40:32 -0500

    Fix typo

--- Tass/codyhichar.asm
+++ Tass/codyhichar.asm
@@ -1,6 +1,6 @@
 ;
 ; codyhichar.asm
-; A high-reolution character mode demo displaying a PETSCII Commodore 64 graphic.
+; A high-resolution character mode demo displaying a PETSCII Commodore 64 graphic.
 ;
 ; The image is from "The Game Is Apaw!" by iLKke. The C64 font is taken
 ; from the US C64 character ROM. Copyright on the image and ROM remain
